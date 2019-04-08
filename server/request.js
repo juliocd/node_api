eeccfe2d979238e8b26645461a379a86e76b83b9
juliodiaz <julio@vimbly.com>
2019-04-08 09:44:50 -0500

    Add promise

--- server/request.js
+++ server/request.js
@@ -6,6 +6,7 @@ var util = require('./util.js');
 const bodyParser = require('body-parser')
 // Here we import our Logger file and instantiate a logger object
 var logger = require("./logger").Logger;
+var Promise = require('promise');
 
 module.exports = function (app){
 
@@ -20,17 +21,36 @@ module.exports = function (app){
     });
 
     app.post('/sms-notifications', bodyParser.json(), bodyParser.urlencoded({ extended: false }), function(req, res) {
-        try{
-            console.log(req.is('text/*'));
-            console.log(req.is('json'));
-            console.log('B: ' + JSON.stringify(req.body));
-            res.status(200).json('Success');
-        }catch(err){
-            logger.error("Error >>>");
-            logger.error(err);
-            logger.error("Error <<<");
-            res.status(500).json('Error')
-        }
+        let body = ''
+
+        req.on('data', (chunk) => {
+            body += chunk.toString()
+        })
+
+        req.on('end', () => {
+            let payload = JSON.parse(body)
+
+            if (payload.Type === 'SubscriptionConfirmation') {
+                logger.error(">>>>" + payload.SubscribeURL);
+                console.log('payload', payload)
+            const promise = new Promise((resolve, reject) => {
+                const url = payload.SubscribeURL
+
+                request(url, (error, response) => {
+                if (!error && response.statusCode == 200) {
+                    console.log('Yess! We have accepted the confirmation from AWS')
+                    return resolve()
+                } else {
+                    return reject()
+                }
+                })
+            })
+
+            promise.then(() => {
+                res.end("ok")
+            })
+            }
+        })
     });
 
     app.post('/sns', bodyParser.json(), bodyParser.urlencoded({ extended: false }), function(req, res) {
